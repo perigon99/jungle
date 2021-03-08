085a12b70398077acf448e396cdc66d166dd8847
Sebastien <perigon@hotmail.com>
2021-03-08 19:58:39 +0000

    validation an test done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, length: { minimum: 6, maximum: 20 }, on: :create
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+
+  validates :password, confirmation: true
 end
