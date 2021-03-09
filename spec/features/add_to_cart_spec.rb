b92f78ae4c32a91f8b4492019c582ceb2b3466e6
Sebastien <perigon@hotmail.com>
2021-03-09 20:51:30 +0000

    check if cart add element correctly

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts",type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "Click on a product to see it added to cart" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot('test3.png')
+    click_button('Add', match: :first)
+    # click_on "Details", :first
+    sleep(1)
+    save_screenshot('test3b.png')
+    page.has_content?(' My Cart (1)')
+  end
+end
